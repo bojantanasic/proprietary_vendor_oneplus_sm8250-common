32123d88d411f7c47a6d4f2db4a39cacc5bac901
Michael Bestas <mkbestas@lineageos.org>
2025-12-27 17:04:02 +0200

    sm8250-common: Remove machine learning blobs
    * liblearningmodule loading is guarded under a debug prop
      in qti perf HAL and libmeters is a liblearningmodule dependency
    
    Change-Id: I7497b7c286c3ab6a6aeea908cf38863b0c60d757

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -1961,7 +1961,6 @@ PRODUCT_PACKAGES += \
     libkeymasterprovision \
     libkeymasterutils \
     liblbs_core \
-    liblearningmodule \
     libloc_api_v02 \
     libloc_core \
     libloc_socket \
@@ -1972,7 +1971,6 @@ PRODUCT_PACKAGES += \
     liblowi_wifihal \
     liblqe \
     libmdmimgload \
-    libmeters \
     libmidasserviceintf \
     libmidasserviceintf_aidl \
     libminkdescriptor \
